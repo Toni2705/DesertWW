8cf239d31caab907aea240975240ebee86439441
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2024-03-05 12:04:18 +0100

    AdminMenu

--- DeserWW/resources/views/headerAdmin.blade.php
+++ DeserWW/resources/views/headerAdmin.blade.php
@@ -3,11 +3,6 @@
         <h2>CONTROL DE ADMINISTRACIÓN</h2>
     </div>
     <div>
-        <?php
-            $imagePath = public_path('images/icono.png');
-
-        ?>
-
         <img src="{{ asset('images/icono.png') }}" alt="Icono">
     </div>
 </header>
--- DeserWW/resources/views/menuadmin.blade.php
+++ DeserWW/resources/views/menuadmin.blade.php
@@ -6,7 +6,15 @@
     <title>Admin</title>
 </head>
 @include('headerAdmin')
-<body>
-
+<body id="principalAdmin">
+    <div>
+    <div> <img src="{{ asset('images/carrera.jpg') }}" alt="Icono"></div>
+    <div> <img src="{{ asset('images/pilotos.jpg') }}" alt="Icono"></div>
+    <div><img src="{{ asset('images/logo.png') }}" alt="Icono"></div>
+    <div><img src="{{ asset('images/nike.png') }}" alt="Icono"></div>
+    <div> <img src="{{ asset('images/dkv.png') }}" alt="Icono"></div>
+    
+    </div>    
+    
 </body>
 </html>
