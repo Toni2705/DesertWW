bec20fed5d1bdef5e628e756bccee37699dff86e
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2024-03-05 14:18:29 +0100

    MenuPrincipal

--- DeserWW/resources/views/headerAdmin.blade.php
+++ DeserWW/resources/views/headerAdmin.blade.php
@@ -1,8 +1,22 @@
+<body>
 <header>
-    <div id="header">
-        <h2>CONTROL DE ADMINISTRACIÓN</h2>
-    </div>
-    <div>
-        <img src="{{ asset('images/icono.png') }}" alt="Icono">
-    </div>
-</header>
+        <div id="header" class="d-flex align-items-center justify-content-between px-3 py-2 bg-dark">
+            <img src="{{ asset('images/logo.png') }}" alt="Icono" class="me-3">
+            <h2 class="m-0">CONTROL DE ADMINISTRACIÓN</h2>
+        </div>
+    </header>
+<style>
+    img {
+        width: 80px;
+        height: 50px;
+    }
+    #header{
+        height: 70px;
+
+    }
+    h2 {
+        color: white;
+        text-align: center;
+        flex-grow: 1;
+    }
+</style>
--- DeserWW/resources/views/menuadmin.blade.php
+++ DeserWW/resources/views/menuadmin.blade.php
@@ -3,18 +3,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Admin</title>
+    <style>
+        /* Estilos personalizados */
+        body{
+            overflow-y: hidden;
+            margin: 0;
+        }
+
+        #contenidoPrincipal {
+            background-image: url('{{ asset("images/fondo.png") }}'); 
+            background-size: cover; /* La imagen de fondo cubrirá todo el contenedor sin repetirse */
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 20px;
+            height: 100%;
+            text-align: center; /* Centra horizontalmente el contenido */
+        }
+        .imagen {
+            margin-bottom: 20px;
+        }
+        .img{
+            width: 250px;
+            height: 310px;
+            border-radius: 10%;
+            border-style: solid ;
+            border-color: black;
+        }
+    </style>
 </head>
 @include('headerAdmin')
-<body id="principalAdmin">
-    <div>
-    <div> <img src="{{ asset('images/carrera.jpg') }}" alt="Icono"></div>
-    <div> <img src="{{ asset('images/pilotos.jpg') }}" alt="Icono"></div>
-    <div><img src="{{ asset('images/logo.png') }}" alt="Icono"></div>
-    <div><img src="{{ asset('images/nike.png') }}" alt="Icono"></div>
-    <div> <img src="{{ asset('images/dkv.png') }}" alt="Icono"></div>
-    
-    </div>    
-    
+    <div id="contenidoPrincipal">
+        <div class="container mx-auto">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="imagen">
+                        <img src="{{ asset('images/pilotos.png') }}" alt="Icono" class="img">
+                    </div>
+                    <div class="imagen">
+                        <img src="{{ asset('images/nike.png') }}" alt="Icono" class="img">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="imagen">
+                        <img src="{{ asset('images/carrera.jpg') }}" alt="Icono" class="img">
+                    </div>
+                    <div class="imagen">
+                        <img src="{{ asset('images/dkv.png') }}" alt="Icono" class="img">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
