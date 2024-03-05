108ccfaebcd208605cf0f43d850e842df6ccf64c
Antoni Rubio Santiago <antoni.rubio@inslapineda.cat>
2024-03-05 10:31:41 +0100

    Proyecto

---
+++ DeserWW/resources/views/headerAdmin.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <div id="header">
+        <h2>CONTROL DE ADMINISTRACIÓN</h2>
+    </div>
+    <div>
+        <img src="../../public/images/user-icon-on-transparent-background-free-png.png" alt="Imagen Usuario">
+    </div>
+</header>
