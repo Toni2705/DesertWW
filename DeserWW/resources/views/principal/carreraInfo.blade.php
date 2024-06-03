a9d1ea6992b556f217f74f6c1ed4e71f94b92c2a
Toni2705 <antoni.rubio@inslapineda.cat>
2024-06-03 19:41:31 +0200

    Paypal y arreglos

--- DeserWW/resources/views/principal/carreraInfo.blade.php
+++ DeserWW/resources/views/principal/carreraInfo.blade.php
@@ -13,14 +13,13 @@ $idCorredor = Auth::id();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <title>{{ $carrera->nombre }}</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 
-<style><div id="paypal-button-container" style="display: none;"></div>
+<style>
     .imgCartel {
         width: 270px;
         height: 450px;
