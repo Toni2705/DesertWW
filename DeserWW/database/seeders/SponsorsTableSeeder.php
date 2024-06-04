991c2e65beda88d64f86141908965bc096903874
Toni2705 <antoni.rubio@inslapineda.cat>
2024-06-04 19:55:36 +0200

    css menu

--- DeserWW/database/seeders/SponsorsTableSeeder.php
+++ DeserWW/database/seeders/SponsorsTableSeeder.php
@@ -16,7 +16,7 @@ class SponsorsTableSeeder extends Seeder
         DB::table('sponsors')->insert([
             'nombre' => 'Nike',
             'CIF' => 'GHI101',
-            'logo' => 'sponsors/logo_nike.png',
+            'logo' => 'images/sponsors/logo_nike.png',
             'direccion' => 'Plaza Central 789',
             'principal' => true,
             'created_at' => now(),
@@ -26,7 +26,7 @@ class SponsorsTableSeeder extends Seeder
         DB::table('sponsors')->insert([
             'nombre' => 'HP',
             'CIF' => 'JKL012',
-            'logo' => 'sponsors/logo_hp.png',
+            'logo' => 'images/sponsors/logo_hp.png',
             'direccion' => 'Esquina Norte 012',
             'principal' => false,
             'created_at' => now(),
@@ -36,7 +36,7 @@ class SponsorsTableSeeder extends Seeder
         DB::table('sponsors')->insert([
             'nombre' => 'Adidas',
             'CIF' => 'GHI709',
-            'logo' => 'sponsors/logo_adidas.png',
+            'logo' => 'images/sponsors/logo_adidas.png',
             'direccion' => 'Plaza Toro 789',
             'principal' => false,
             'created_at' => now(),
@@ -46,7 +46,7 @@ class SponsorsTableSeeder extends Seeder
         DB::table('sponsors')->insert([
             'nombre' => 'Audi',
             'CIF' => 'JKL032',
-            'logo' => 'sponsors/logo_audi.png',
+            'logo' => 'images/sponsors/logo_audi.png',
             'direccion' => 'Esquina Sur 012',
             'principal' => true,
             'created_at' => now(),
@@ -56,7 +56,7 @@ class SponsorsTableSeeder extends Seeder
         DB::table('sponsors')->insert([
             'nombre' => 'Ferrari',
             'CIF' => 'GHI289',
-            'logo' => 'sponsors/logo_ferrari.png',
+            'logo' => 'images/sponsors/logo_ferrari.png',
             'direccion' => 'Plaza Principal 789',
             'principal' => true,
             'created_at' => now(),
